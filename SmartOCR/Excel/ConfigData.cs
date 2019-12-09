acb1f5c73c425e17e2084130d898b3baa7338e23
Maksim.Chuvashov <Maksim.Chuvashov@RU3012722W3>
2019-12-09 18:58:17 +0700

    Added template for config data parsing

---
+++ SmartOCR/Excel/ConfigData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartOCR.Excel
+{
+    class ConfigData
+    {
+        public List<ConfigField> fields { get; }
+
+        public ConfigField this[int index]
+        {
+            get
+            {
+                return fields[index];
+            }
+        }
+
+        public ConfigField this[string index]
+        {
+            get
+            {
+                return fields.Where(single_field => single_field.Name == index).First();
+            }
+        }
+
+        private ConfigData()
+        {
+            fields = new List<ConfigField>();
+        }
+
+        public ConfigData(params ConfigField[] fields) : this()
+        {
+            foreach (ConfigField item in fields)
+            {
+                if (item != null)
+                {
+                    this.fields.Add(item);
+                }
+            }
+            
+        }
+
+        public ConfigData(IEnumerable<ConfigField> fields) : this()
+        {
+            this.fields = fields.ToList();
+        }
+
+        public void AddField(ConfigField field)
+        {
+            if (field != null)
+            {
+                this.fields.Add(field);
+            }
+            else
+            {
+                throw new ArgumentNullException("Null config field was provided.");
+            }
+        }
+
+
+
+
+
+    }
+
+
+}
---
+++ SmartOCR/Excel/ConfigField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartOCR.Excel
+{
+    class ConfigField
+    {
+        public string ValueType { get; }
+        public string RE_Pattern { get; }
+        public string Name { get; }
+        public string ExpectedName { get; }
+        public List<ConfigExpression> Expressions { get; }
+
+        private ConfigField()
+        {
+            Expressions = new List<ConfigExpression>();
+        }
+
+        public ConfigField(string name, string value_type) : this()
+        {
+            this.Name = name;
+            this.ValueType = value_type;
+        }
+
+        public void ParseFieldExpression(string input)
+        {
+            
+        }
+
+        public void AddSearchExpression(ConfigExpression expression)
+        {
+            this.Expressions.Add(expression);
+        }
+
+    }
+}
