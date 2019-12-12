5b7ddd46d76b5f81813065db85cd1fcd20863eec
Maksim.Chuvashov <Maksim.Chuvashov@RU3012722W3>
2019-12-12 16:16:23 +0700

    Fixed case when semicolon was part of RE pattern

--- SmartOCR/Excel/ConfigExpression.cs
+++ SmartOCR/Excel/ConfigExpression.cs
@@ -31,6 +31,15 @@ namespace SmartOCR
         private void ParseInput(string input)
         {
             var splitted_input = input.Split(';').ToList();
+            while (!(int.TryParse(splitted_input[1], out _) || string.IsNullOrEmpty(splitted_input[1])))
+            {
+                splitted_input[0] = $"{splitted_input[0]};{splitted_input[1]}";
+                for (int i = 2; i < splitted_input.Count; i++)
+                {
+                    splitted_input[i - 1] = splitted_input[i];
+                }
+                splitted_input.RemoveAt(splitted_input.Count - 1);
+            }
             while (splitted_input.Count < 3)
             {
                 splitted_input.Add(null);
