2b676ead2867f3a82bf22289311478e4c5ac183f
Maksim.Chuvashov <Maksim.Chuvashov@RU3012722W3>
2019-12-13 09:36:37 +0700

    Added summary for CMDProcess class

--- SmartOCR/CMDProcess.cs
+++ SmartOCR/CMDProcess.cs
@@ -5,20 +5,54 @@ using System.Linq;
 
 namespace SmartOCR
 {
+    /// <summary>
+    /// CMDProcess is used to process data from command prompt: define document type and files to parse.
+    /// </summary>
     public class CMDProcess : IProcess
     {
-        private string DocumentType { get; }
-        private string PathType { get; }
-        private IEnumerable<string> Args { get; }
-        public bool ReadyToProcess { get; }
+        #region Fields
+        /// <summary>
+        /// Specifies the type of entered command propmt arguments
+        /// </summary>
+        private enum PathType : int
+        {
+            None,
+            Directory,
+            File
+        }
+        /// <summary>
+        /// Title of document type.
+        /// </summary>
+        private readonly string document_type;
+        /// <summary>
+        /// Specification of entered arguments.
+        /// </summary>
+        private readonly PathType entered_path_type;
+        /// <summary>
+        /// Collection of entered command propmpt arguments.
+        /// </summary>
+        private readonly List<string> _args;
+        #endregion
 
+        #region Properties
+        /// <summary>
+        /// Indicates whether command prompt arguments were successfully defined.
+        /// </summary>
+        public bool ReadyToProcess { get; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes CMD processing object: identifies document type, type of entered paths and files by those paths.
+        /// </summary>
+        /// <param name="args">Array of command prompt arguments.</param>
         public CMDProcess(string[] args)
         {
             try
             {
-                DocumentType = ValidateDocumentType(args[0]);
-                PathType = ValidatePath(args[1]);
-                this.Args = args.Skip(1);
+                document_type = ValidateDocumentType(args[0]);
+                entered_path_type = ValidatePath(args[1]);
+                _args = args.Skip(1).ToList();
                 ReadyToProcess = true;
             }
             catch (IndexOutOfRangeException)
@@ -27,58 +61,83 @@ namespace SmartOCR
                 ReadyToProcess = false;
             }
         }
+        #endregion
 
+        #region Public methods
+        /// <summary>
+        /// Calls for processing of identified documents.
+        /// </summary>
         public void ExecuteProcessing()
         {
-            if (DocumentType == null || PathType == null)
+            if (document_type == null || entered_path_type == PathType.None)
             {
                 Utilities.PrintInvalidInputMessage();
                 return;
             }
-            using (var entryPoint = new ParseEntryPoint(DocumentType, GetFilesFromArgs()))
+            using (var entryPoint = new ParseEntryPoint(document_type, GetFilesFromArgs()))
             {
                 entryPoint.TryGetData();
             }
         }
+        #endregion
 
+        #region Private methods
+        /// <summary>
+        /// Processes entered path type and path argument(s).
+        /// </summary>
+        /// <returns>List of files, specified by arguments.</returns>
         private List<string> GetFilesFromArgs()
         {
-            if (PathType == "directory")
+            if (entered_path_type == PathType.Directory)
             {
                 return GetFilesFromDirectories();
             }
-            return new List<string>(Args);
+            return new List<string>(_args);
         }
-
+        /// <summary>
+        /// Gets files from directories, provided in Args field.
+        /// Files with .docx extension are taken.
+        /// Files with '~' symbol in title are ignored.
+        /// </summary>
+        /// <returns>Collection of files in provided directories.</returns>
         private List<string> GetFilesFromDirectories()
         {
             List<string> directories = new List<string>();
-            foreach (var item in Args)
+            for (int i = 0; i < _args.Count; i++)
             {
-                directories.AddRange(Directory.GetFiles(item).Where(file => !file.Contains("~")));
+                string single_directory = _args[i];
+                directories.AddRange(Directory.GetFiles(single_directory).Where(item => !item.StartsWith("~") && item.EndsWith(".docx")));
             }
             return directories;
         }
-
-        public string ValidateDocumentType(string doc_type)
+        /// <summary>
+        /// Checks whether provided document type is in compliance with supportable document types.
+        /// </summary>
+        /// <param name="doc_type">Document type, entered from command propmt.</param>
+        /// <returns>String representation of internally defined document type.</returns>
+        private string ValidateDocumentType(string doc_type)
         {
             return Utilities.valid_document_types.FirstOrDefault(item => item.Contains(doc_type));
         }
 
-        public string ValidatePath(string path)
+        /// <summary>
+        /// Checks whether argument, provided after document type is path to directory or file.
+        /// </summary>
+        /// <param name="path">Path, entered from command prompt.</param>
+        /// <returns>PathType enumeration item, that indicates path type.</returns>
+        private PathType ValidatePath(string path)
         {
             if (Directory.Exists(path))
             {
-                return "directory";
+                return PathType.Directory;
             }
-            else if (File.Exists(path))
+            if (File.Exists(path))
             {
-                return "file";
+                return PathType.File;
             }
-            else
-            {
-                return null;
-            }
+            return PathType.None;
         }
+        #endregion
+
     }
 }
