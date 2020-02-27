9cb818b33aad49d786229d7faabba8baccb7265b
Maksim.Chuvashov <Maksim.Chuvashov@RU3012722W3>
2020-02-27 10:15:16 +0700

    Added summary for TreeNodeContent class

--- SmartOCR/Search/TreeNodeContent.cs
+++ SmartOCR/Search/TreeNodeContent.cs
@@ -3,13 +3,25 @@
     using System;
     using System.Collections.Generic;
 
-    public class TreeNodeContent // TODO: add summary.
+    /// <summary>
+    /// Class represents description of TreeNode search parameters.
+    /// </summary>
+    public class TreeNodeContent
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreeNodeContent"/> class.
+        /// Properties are initialized with default values.
+        /// </summary>
         public TreeNodeContent()
         {
             this.Lines = new List<long>();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreeNodeContent"/> class.
+        /// Properties are initialized with values from other <see cref="TreeNodeContent"/> instance properties.
+        /// </summary>
+        /// <param name="content">A source instance of the <see cref="TreeNodeContent"/> class.</param>
         public TreeNodeContent(TreeNodeContent content)
             : this()
         {
@@ -32,26 +44,59 @@
             this.Lines.AddRange(content.Lines);
         }
 
+        /// <summary>
+        /// Gets or sets value to check with regular expression.
+        /// </summary>
         public string CheckValue { get; set; }
 
+        /// <summary>
+        /// Gets or sets value that is matched by regular expression.
+        /// </summary>
         public string FoundValue { get; set; }
 
+        /// <summary>
+        /// Gets or sets horizontal location of paragraph where text match was found.
+        /// </summary>
         public decimal HorizontalParagraph { get; set; }
 
-        public int SecondSearchParameter { get; set; }
-
+        /// <summary>
+        /// Gets or sets primary numeric parameter of search expression.
+        /// </summary>
         public int FirstSearchParameter { get; set; }
 
+        /// <summary>
+        /// Gets or sets secondary numeric parameter of search expression.
+        /// </summary>
+        public int SecondSearchParameter { get; set; }
+
+        /// <summary>
+        /// Gets collection of document lines where match was found.
+        /// </summary>
         public List<long> Lines { get; }
 
+        /// <summary>
+        /// Gets or sets field name.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets TreeNode type.
+        /// </summary>
         public string NodeLabel { get; set; }
 
+        /// <summary>
+        /// Gets or sets regular expression pattern that is used to match text.
+        /// </summary>
         public string RegExPattern { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether match has been found.
+        /// </summary>
         public bool Status { get; set; }
 
+        /// <summary>
+        /// Gets or sets value type of searched value.
+        /// </summary>
         public string ValueType { get; set; }
     }
 }
