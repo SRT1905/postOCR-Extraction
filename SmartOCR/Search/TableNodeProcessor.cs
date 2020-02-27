547dd2ceb370ae26a96c0daae2351e9a5ed5314c
Maksim.Chuvashov <Maksim.Chuvashov@RU3012722W3>
2020-02-27 14:11:13 +0700

    Refactored TableNodeProcessor class

--- SmartOCR/Search/TableNodeProcessor.cs
+++ SmartOCR/Search/TableNodeProcessor.cs
@@ -4,17 +4,28 @@
     using System.Collections.Generic;
     using System.Text.RegularExpressions;
 
+    /// <summary>
+    /// Used to search values, specified by configuration fields, in Word tables.
+    /// </summary>
     public class TableNodeProcessor
     {
         private readonly List<WordTable> tables;
         private readonly TreeNode tableNode;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TableNodeProcessor"/> class.
+        /// </summary>
+        /// <param name="tableCollection">A collection of formatted Word tables.</param>
+        /// <param name="fieldNode">A search node, containing information about search field.</param>
         public TableNodeProcessor(List<WordTable> tableCollection, TreeNode fieldNode)
         {
             this.tables = tableCollection;
             this.tableNode = fieldNode;
         }
 
+        /// <summary>
+        /// Loops through every Word table searching for specified values.
+        /// </summary>
         public void Process()
         {
             for (int i = 0; i < this.tables.Count; i++)
@@ -29,18 +40,26 @@
         private static bool ExtractValueFromMatch(TreeNode childNode, string itemByExpressionPosition, Regex regexObject)
         {
             int slashCharIndex = childNode.Content.ValueType.IndexOf("/", StringComparison.OrdinalIgnoreCase) + 1;
-            string nestedValueType = slashCharIndex == 0
+            string nestedValueType = GetTableValueType(childNode, slashCharIndex);
+            TryToValidateMatch(childNode, new MatchProcessor(itemByExpressionPosition, regexObject, nestedValueType));
+            return true;
+        }
+
+        private static string GetTableValueType(TreeNode childNode, int slashCharIndex)
+        {
+            return slashCharIndex == 0
                 ? "String"
                 : childNode.Content.ValueType.Substring(slashCharIndex);
-            var matchProcessor = new MatchProcessor(itemByExpressionPosition, regexObject, nestedValueType);
+        }
+
+        private static void TryToValidateMatch(TreeNode childNode, MatchProcessor matchProcessor)
+        {
             if (!string.IsNullOrEmpty(matchProcessor.Result))
             {
                 childNode.Content.FoundValue = matchProcessor.Result;
                 childNode.Content.Status = true;
                 PropagateStatusInTree(true, childNode);
             }
-
-            return true;
         }
 
         private static bool TryToFindMatchInTable(WordTable table, Regex regexObject, string checkValue)
@@ -49,21 +68,21 @@
             {
                 for (int j = 0; j < table.ColumnCount; j++)
                 {
-                    if (table[i, j] != null && regexObject.IsMatch(table[i, j]))
+                    if (ProcessSingleCell(table[i, j], regexObject, checkValue))
                     {
-                        Match singleMatch = regexObject.Match(table[i, j]);
-                        int index = Convert.ToInt32(singleMatch.Groups.Count > 0);
-                        var similarity = new SimilarityDescription(singleMatch.Groups[index].Value, checkValue);
-
-                        if (similarity.CheckStringSimilarity())
-                        {
-                            return true;
-                        }
+                        return true;
                     }
                 }
             }
 
             return false;
+        }
+
+        private static bool ProcessSingleCell(string cellValue, Regex regexObject, string checkValue)
+        {
+            Match singleMatch = regexObject.Match(cellValue);
+            int index = Convert.ToInt32(singleMatch.Groups.Count > 0);
+            return new SimilarityDescription(singleMatch.Groups[index].Value, checkValue).CheckStringSimilarity();
         }
 
         private static void PropagateStatusInTree(bool status, TreeNode node)
@@ -76,31 +95,26 @@
             }
         }
 
+        private static string GetCellByNodeContent(WordTable table, TreeNode childNode)
+        {
+            return table[childNode.Content.FirstSearchParameter, childNode.Content.SecondSearchParameter];
+        }
+
         private bool TryGetDataFromTable(WordTable table)
         {
-            if (!TryToFindMatchInTable(
-                table,
-                Utilities.CreateRegexpObject(this.tableNode.Content.RegExPattern),
-                this.tableNode.Content.CheckValue))
-            {
-                return false;
-            }
-
-            return this.ProcessNodeData(table);
+            return TryToFindMatchInTable(table, Utilities.CreateRegexpObject(this.tableNode.Content.RegExPattern), this.tableNode.Content.CheckValue)
+                ? this.ProcessNodeData(table)
+                : false;
         }
 
         private bool ProcessNodeData(WordTable table)
         {
             TreeNode childNode = this.GetTerminalNode();
-
-            string itemByExpressionPosition = table[childNode.Content.FirstSearchParameter, childNode.Content.SecondSearchParameter];
+            string itemByExpressionPosition = GetCellByNodeContent(table, childNode);
             Regex regexObject = Utilities.CreateRegexpObject(childNode.Content.RegExPattern);
-            if (regexObject.IsMatch(itemByExpressionPosition))
-            {
-                return ExtractValueFromMatch(childNode, itemByExpressionPosition, regexObject);
-            }
-
-            return false;
+            return regexObject.IsMatch(itemByExpressionPosition)
+                ? ExtractValueFromMatch(childNode, itemByExpressionPosition, regexObject)
+                : false;
         }
 
         private TreeNode GetTerminalNode()
