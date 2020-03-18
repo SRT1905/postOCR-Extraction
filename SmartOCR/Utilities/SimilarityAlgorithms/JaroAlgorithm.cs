37672a51646d11330842d0c17151a5560c5191ac
Maksim.Chuvashov <Maksim.Chuvashov@RU3012722W3>
2020-03-18 17:37:43 +0700

    Added Jaro and Jaro-Wrinkler string similarity algorithms

---
+++ SmartOCR/Utilities/SimilarityAlgorithms/JaroAlgorithm.cs
@@ -0,0 +1,135 @@
+﻿namespace SmartOCR
+{
+    using System;
+
+    /// <summary>
+    /// Contains methods to calculate string similarity measure using Jaro similarity.
+    /// </summary>
+    public class JaroAlgorithm : ISimilarityAlgorithm
+    {
+        /// <inheritdoc/>
+        public virtual double GetStringSimilarity(string leftString, string rightString)
+        {
+            return GetJaroSimilarity(leftString, rightString);
+        }
+
+        /// <summary>
+        /// Calculates similarity ratio between strings using Jaro similarity measure.
+        /// </summary>
+        /// <param name="leftString">First compared string.</param>
+        /// <param name="rightString">Second compared string.</param>
+        /// <returns>Similarity ratio between two strings.</returns>
+        protected static double GetJaroSimilarity(string leftString, string rightString)
+        {
+            return leftString != rightString
+                ? leftString.Length != 0 && rightString.Length != 0
+                    ? GetSimilarityFromValidStrings(leftString, rightString)
+                    : 0.0
+                : 1.0;
+        }
+
+        private static double GetSimilarityFromValidStrings(string leftString, string rightString)
+        {
+            // Hash for matches
+            int[] hashLeft = new int[leftString.Length];
+            int[] hashRight = new int[rightString.Length];
+            int matchCount = GetMatchCount(leftString, rightString, hashLeft, hashRight);
+            return matchCount == 0
+                ? 0.0
+                : CalculateSimilarityRatio(
+                    leftString.Length,
+                    rightString.Length,
+                    matchCount,
+                    GetCountOfTranspositions(leftString, rightString, hashLeft, hashRight));
+        }
+
+        private static double CalculateSimilarityRatio(int leftStringLength, int rightStringLength, int matchCount, double transpositions)
+        {
+            return (((double)matchCount / leftStringLength)
+                + ((double)matchCount / rightStringLength)
+                + (((double)matchCount - transpositions) / ((double)matchCount))) / 3.0;
+        }
+
+        private static int GetMatchCount(string leftString, string rightString, int[] hashLeft, int[] hashRight)
+        {
+            int matchCount = 0;
+            for (int i = 0; i < leftString.Length; i++)
+            {
+                matchCount += GetCountOfMatchesWithSingleChar(leftString, rightString, hashLeft, hashRight, i);
+            }
+
+            return matchCount;
+        }
+
+        private static int GetCountOfMatchesWithSingleChar(string leftString, string rightString, int[] hashLeft, int[] hashRight, int charIndex)
+        {
+            Tuple<int, int> indexes = GetIndexesForMatchSearch(
+                charIndex,
+                GetMaxDistance(leftString, rightString),
+                rightString.Length);
+            for (int j = indexes.Item1; j < indexes.Item2; j++)
+            {
+                if (leftString[charIndex] == rightString[j] && hashRight[j] == 0)
+                {
+                    return ReturnSuccessfulMatch(hashLeft, hashRight, charIndex, j);
+                }
+            }
+
+            return 0;
+        }
+
+        private static int ReturnSuccessfulMatch(int[] hashLeft, int[] hashRight, int leftCharIndex, int rightCharIndex)
+        {
+            hashLeft[leftCharIndex] = 1;
+            hashRight[rightCharIndex] = 1;
+            return 1;
+        }
+
+        private static Tuple<int, int> GetIndexesForMatchSearch(int leftStringCharIndex, int maxDistance, int rightStringLength)
+        {
+            return new Tuple<int, int>(
+                Math.Max(0, leftStringCharIndex - maxDistance),
+                Math.Min(rightStringLength, leftStringCharIndex + maxDistance + 1));
+        }
+
+        private static int GetMaxDistance(string leftString, string rightString)
+        {
+            // Maximum distance up to which matching is allowed
+            return (int)Math.Floor((double)Math.Max(leftString.Length, rightString.Length) / 2) - 1;
+        }
+
+        private static double GetCountOfTranspositions(string leftString, string rightString, int[] hashLeft, int[] hashRight)
+        {
+            // Number of transpositions
+            double transpositions = 0;
+            int point = 0;
+
+            // Count number of cases when two characters match but there is a third matched char between the indexes
+            for (int i = 0; i < leftString.Length; i++)
+            {
+                if (hashLeft[i] == 1)
+                {
+                    point = FindMatchingPointInSecondString(hashRight, point);
+
+                    if (leftString[i] != rightString[point++])
+                    {
+                        transpositions++;
+                    }
+                }
+            }
+
+            return transpositions / 2;
+        }
+
+        private static int FindMatchingPointInSecondString(int[] hashRight, int point)
+        {
+            // Find next matches char in second string
+            while (hashRight[point] == 0)
+            {
+                point++;
+            }
+
+            return point;
+        }
+    }
+}
---
+++ SmartOCR/Utilities/SimilarityAlgorithms/JaroWrinklerAlgorithm.cs
@@ -0,0 +1,50 @@
+﻿namespace SmartOCR
+{
+    using System;
+
+    /// <summary>
+    /// Contains methods to calculate string similarity measure using Jaro-Wrinkler similarity.
+    /// </summary>
+    public class JaroWrinklerAlgorithm : JaroAlgorithm
+    {
+        private const double JaroSimilarityThreshold = 0.7;
+
+        /// <inheritdoc/>
+        public override double GetStringSimilarity(string leftString, string rightString)
+        {
+            return GetJaroWrinklerDistance(leftString, rightString);
+        }
+
+        private static double GetJaroWrinklerDistance(string leftString, string rightString)
+        {
+            double jaroDistance = GetJaroSimilarity(leftString, rightString);
+
+            return jaroDistance > JaroSimilarityThreshold
+                ? CalculateJaroWrinklerMeasure(leftString, rightString, jaroDistance)
+                : jaroDistance;
+        }
+
+        private static double CalculateJaroWrinklerMeasure(string leftString, string rightString, double jaroDistance)
+        {
+            return jaroDistance + (0.1 * GetLengthOfCommonPrefix(leftString, rightString) * (1 - jaroDistance));
+        }
+
+        private static int GetLengthOfCommonPrefix(string leftString, string rightString)
+        {
+            int prefix = 0;
+            for (int i = 0; i < Math.Max(4, Math.Min(leftString.Length, rightString.Length)); i++)
+            {
+                if (leftString[i] == rightString[i])
+                {
+                    prefix++;
+                }
+                else
+                {
+                    return prefix;
+                }
+            }
+
+            return prefix;
+        }
+    }
+}
