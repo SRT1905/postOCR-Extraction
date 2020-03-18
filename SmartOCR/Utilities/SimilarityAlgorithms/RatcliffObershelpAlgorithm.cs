67110dfcdcf40387bad9bffcf818270c3236d09f
Maksim.Chuvashov <Maksim.Chuvashov@RU3012722W3>
2020-03-18 18:10:54 +0700

    Added Ratcliff-Obershelp string similarity algorithm

---
+++ SmartOCR/Utilities/SimilarityAlgorithms/RatcliffObershelpAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartOCR
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Represents class for calculating string similarity using Ratcliff-Obershelp algorithm.
+    /// </summary>
+    public class RatcliffObershelpAlgorithm : ISimilarityAlgorithm
+    {
+        /// <inheritdoc/>
+        public double GetStringSimilarity(string leftString, string rightString)
+        {
+            return 2 * Convert.ToDouble(leftString.Intersect(rightString).Count()) /
+                Convert.ToDouble(leftString.Length + rightString.Length);
+        }
+    }
+}
